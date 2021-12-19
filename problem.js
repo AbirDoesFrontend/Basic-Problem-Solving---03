63bfd86141f8dc9a88e034db8d87be9aeb95c03e
Abir <ak.cr852456@gmail.com>
2021-12-20 02:41:26 +0600

    Java Basic Problem 3

---
+++ problem.js
@@ -0,0 +1,38 @@
+var num_minusOne= 10;
+var num_minusZero= 11;
+var num_zero= 12;
+var num_one= 13;
+var num_two= 14;
+var num_three= 15;
+var num_four= 16;
+var num_five= 17;
+var num_six= 18;
+var num_seven= 19;
+var num_eight= 20;
+
+if(num_minusOne, num_eight % 10 == 0){
+ console.log(num_minusOne + " is divisible")
+ console.log(num_minusZero + " is divisible")
+ console.log(num_zero + " is divisible")
+ console.log(num_one + " is divisible")
+ console.log(num_two + " is divisible")
+ console.log(num_three + " is divisible")
+ console.log(num_four + " is divisible")
+ console.log(num_five + " is divisible")
+ console.log(num_six + " is divisible")
+ console.log(num_seven + " is divisible")
+ console.log(num_eight + " is divisible")
+} else{
+ console.log(num_minusZero + " is not divisible")
+ console.log(num_zero + " is not divisible")
+ console.log(num_one + " is not divisible")
+ console.log(num_two + " is not divisible")
+ console.log(num_three + " is not divisible")
+ console.log(num_four + " is not divisible")
+ console.log(num_five + " is not divisible")
+ console.log(num_six + " is not divisible")
+ console.log(num_seven + " is not divisible")
+}
+
+
+
